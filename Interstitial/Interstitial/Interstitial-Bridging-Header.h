a01d4f74cfdb530a41608cc308fef8f92633c3d9
dleonard <dleonard@yahoo-inc.com>
2015-07-29 14:07:45 -0700

    Adding new full-screen ad project

---
+++ Interstitial/Interstitial/Interstitial-Bridging-Header.h
@@ -0,0 +1,21 @@
+//
+//  Interstitial-Bridging-Header.h
+//  Interstitial
+//
+//  Created by David Leonard on 6/11/15.
+//  Copyright (c) 2015 David Leonard. All rights reserved.
+//
+
+#ifndef Interstitial_Interstitial_Bridging_Header_h
+#define Interstitial_Interstitial_Bridging_Header_h
+
+#import "Flurry.h"
+#import "FlurryAdError.h"
+#import "FlurryAdTargeting.h"
+#import "FlurryAds.h"
+#import "FlurryAdDelegate.h"
+
+#import "FlurryAdInterstitial.h"
+#import "FlurryAdInterstitialDelegate.h"
+
+#endif
